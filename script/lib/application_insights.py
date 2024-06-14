8b885e8c59499fd61929d08c92f52de646b60bd5
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2024-06-14 09:09:03 +0100

    Add script to generate domain events from all discovered HMPPS Domain Events

---
+++ script/lib/application_insights.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import json
+import requests
+
+
+def ai_request(query):
+    APP_INSIGHTS_APPLICATION_GUID = os.environ["APP_INSIGHTS_APPLICATION_GUID"]
+    APP_INSIGHTS_API_KEY = os.environ["APP_INSIGHTS_API_KEY"]
+
+    params = {"query": query}
+    headers = {"X-Api-Key": APP_INSIGHTS_API_KEY}
+
+    url = f"https://api.applicationinsights.io/v1/apps/{APP_INSIGHTS_APPLICATION_GUID}/query"
+
+    response = requests.get(url, headers=headers, params=params)
+    return json.loads(response.text)
+
+
+def single_domain_event(event_type):
+
+    # Grab the custom events for a single event type
+    query = f"""customEvents
+         | where cloud_RoleName in ('hmpps-domain-event-logger')
+         | where name == '{event_type}'
+         | where timestamp between((ago(7d)) .. now())
+    """
+
+    full_response = ai_request(query)
+
+    # Do we have any messages?
+    if len(full_response["tables"][0]["rows"]) == 0:
+        sys.exit(
+            f"No domain event message found for '{event_type}' in Application Insights in the previous three days"
+        )
+
+    raw_message = json.loads(full_response["tables"][0]["rows"][0][3])["rawMessage"]
+    single_message_json = json.loads(json.loads(raw_message)["Message"])
+
+    return single_message_json
+
+
+def domain_event_types():
+
+    # Grab all the domain event types from the last week
+    query = f"""customEvents
+         | where cloud_RoleName in ('hmpps-domain-event-logger')
+         | where timestamp between((ago(7d)) .. now())
+         | summarize count() by name
+    """
+
+    full_response = ai_request(query)
+
+    # Do we have any messages?
+    if len(full_response["tables"][0]["rows"]) == 0:
+        sys.exit(
+            f"No domain event messages found in Application Insights in the previous seven days"
+        )
+
+    return [msg[0] for msg in full_response["tables"][0]["rows"]]
--- script/message-spec.py
+++ script/message-spec.py
@@ -3,106 +3,8 @@
 """ Generate an ASyncAPI Message Spec from an HMPPS Domain Event Message """
 
 import argparse
-import os
-import requests
-import sys
-import json
-import yaml
-from collections import OrderedDict
-
-
-def application_insights_request(event_type):
-    APP_INSIGHTS_APPLICATION_GUID = os.environ["APP_INSIGHTS_APPLICATION_GUID"]
-    APP_INSIGHTS_API_KEY = os.environ["APP_INSIGHTS_API_KEY"]
-
-    query = f""" customEvents
-         | where cloud_RoleName in ('hmpps-domain-event-logger')
-         | where name == '{event_type}'
-         | where timestamp between((ago(3d)) .. now())
-    """
-
-    params = {"query": query}
-    headers = {"X-Api-Key": APP_INSIGHTS_API_KEY}
-
-    url = f"https://api.applicationinsights.io/v1/apps/{APP_INSIGHTS_APPLICATION_GUID}/query"
-
-    response = requests.get(url, headers=headers, params=params)
-
-    full_response = json.loads(response.text)
-
-    # Do we have any messages?
-    if len(full_response["tables"][0]["rows"]) == 0:
-        sys.exit(
-            f"No domain event message found for '{event_type}' in Application Insights in the previous three days"
-        )
-
-    raw_message = json.loads(full_response["tables"][0]["rows"][0][3])["rawMessage"]
-    single_message_json = json.loads(json.loads(raw_message)["Message"])
-
-    return single_message_json
-
-
-def api_string(field, value):
-    node = OrderedDict(
-        {
-            "type": "string",
-            "example": value,
-        }
-    )
-
-    return node
-
-
-def api_int(field, value):
-    node = OrderedDict(
-        {
-            "type": "integer",
-            "example": value,
-        }
-    )
-
-    return node
-
-
-def api_object(field, value):
-    node = OrderedDict(
-        {
-            "type": "object",
-            "properties": {},
-        }
-    )
-
-    for sub_field in value:
-        node["properties"][sub_field] = process_field(sub_field, value[sub_field])
-
-    return node
-
-
-def api_list(field, value):
-    node = OrderedDict({"type": "array", "items": []})
-
-    for item in value:
-        node["items"].append(process_field("list_item", item))
-
-    return node
-
-
-def process_field(field, value):
-    type_name = type(value).__name__
-
-    if type_name == "str":
-        field = api_string(field, value)
-
-    if type_name == "int":
-        field = api_int(field, value)
-
-    if type_name == "dict":
-        field = api_object(field, value)
-
-    if type_name == "list":
-        field = api_list(field, value)
-
-    return field
+import lib.asyncapi
+import lib.application_insights
 
 
 if __name__ == "__main__":
@@ -113,25 +15,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Grab a single application insights domain event message
-    message = application_insights_request(args.event)
+    message = lib.application_insights.single_domain_event(args.event)
 
-    new_message = OrderedDict({})
-    new_message["name"] = message["eventType"]
-    new_message["title"] = message["eventType"]
-    new_message["summary"] = message["description"]
-    new_message["contentType"] = "application/json"
-    new_message["payload"] = OrderedDict({"type": "object", "properties": {}})
-
-    for field in message:
-        new_message["payload"]["properties"][field] = process_field(
-            field, message[field]
-        )
-
-    yaml.add_representer(
-        OrderedDict,
-        lambda dumper, data: dumper.represent_mapping(
-            "tag:yaml.org,2002:map", data.items()
-        ),
-    )
-
-    print(yaml.dump(new_message))
+    # Write an ASyncAPI YAML message based on the event
+    print(lib.asyncapi.create_asyncapi_yaml(message))
