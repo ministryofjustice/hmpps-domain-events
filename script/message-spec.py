8e03941ac5e01175ca673693b86d5cab84324eac
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-10-10 09:04:46 +0100

    SDIT-1841: ✨ Add in restricted patients domain events (#48)
    * SDIT-1841: ✨ Add in restricted patients domain events
    
    * Update script/message-spec.py
    
    Co-authored-by: Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
    
    * SDIT-1841: Fix JSON import
    
    ---------
    
    Co-authored-by: Matthew Ryall <matthew.ryall@digital.justice.gov.uk>

--- script/message-spec.py
+++ script/message-spec.py
@@ -5,17 +5,21 @@
 import argparse
 import lib.asyncapi
 import lib.application_insights
-
+import json
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Generate an AsyncAPI message spec from an HMPPS domain event message."
     )
     parser.add_argument("event", type=str, help="An HMPPS domain event type")
+    parser.add_argument("-j", "--json", action="store_true")
     args = parser.parse_args()
 
     # Grab a single application insights domain event message
     message = lib.application_insights.single_domain_event(args.event)
 
-    # Write an ASyncAPI YAML message based on the event
-    print(lib.asyncapi.create_asyncapi_yaml(message))
+    if args.json:
+        print(json.dumps(message))
+    else:
+        # Write an ASyncAPI YAML message based on the event
+        print(lib.asyncapi.create_asyncapi_yaml(message))
